9c08841a428e80b1a212a0a3f7243444fc2cccfb
Максим Захаров <mttpr28@gmail.com>
2020-01-19 12:42:11 +0300

    Documentation added

--- Hello_world/hello_world.java
+++ Hello_world/hello_world.java
@@ -1,6 +1,22 @@
-class A
+/**
+ * 
+ * @author FLTLN
+ * 
+ * Hello, world!
+ * This simple class is used to build-up environment.
+ * So, just first look to Java and Javadoc.
+ * 
+ */
+class HelloWorld
 {
-    public static void main(final String ards[])
+    /**
+     * 
+     * Call "System.out.print" from main.
+     * 
+     * @param args arguments string
+     * 
+     */
+    public static void main(final String args[])
     {
         System.out.print("Hello, world");
     }
